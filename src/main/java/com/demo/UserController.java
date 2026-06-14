b098a9b6b2508dff5b788b4570442c23b6862d1d
akashtinu <yakashtinu@gmail.com>
2026-06-14 16:41:41 +0530

    fix pom.xml

--- src/main/java/com/demo/UserController.java
+++ src/main/java/com/demo/UserController.java
@@ -3,21 +3,20 @@ package com.demo;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.DeleteExchange;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService service;
@@ -25,21 +24,17 @@ public class UserController {
     @GetMapping
     public List<User> getAll() { return service.getAll(); }
 
-//    @PostMapping
-//    public User create(@RequestAttribute User u) { return service.save(u); }
-    
     @PostMapping
     public User create(@RequestBody User u) {
         return service.save(u);
     }
 
-    @DeleteExchange("/{id}")
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) { service.delete(id); }
-    
+
     @PutMapping("/{id}")
     public User update(@PathVariable Long id, @RequestBody User u) {
         u.setId(id);
         return service.save(u);
     }
 }
-
